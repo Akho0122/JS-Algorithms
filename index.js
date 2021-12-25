a234f343dc1a754b02b29a44129b7fa525f96596
Akho0122 <mr.akhror.dev@gmail.com>
2021-12-25 21:29:27 +0500

    Looping Task

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const arr = [1,2,3,4,54,5,6];
+
+function sumArr(arr){
+    let sum = 0
+    for(let i = 0; i < arr.length; i++){
+     sum += arr[i]
+    }
+    console.log(sum)
+}
+
+sumArr(arr)
+
