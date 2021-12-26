8cf4b429e966a6ef70eec347825adf36c683e777
Akho0122 <mr.akhror.dev@gmail.com>
2021-12-26 20:10:47 +0500

    A new task related to for loop was added

--- index.js
+++ index.js
@@ -73,12 +73,14 @@
 // };
 // console.log(tipTwo(false));
 
-function addUp(num) {
-    let sum = 0;
-    for (let i = 1; i <= num; i++) {
-      sum += i;
-    }
-    return sum;
-  }
+// function addUp(num) {
+//     let sum = 0;
+//     for (let i = 1; i <= num; i++) {
+//       sum += i;
+//     }
+//     return sum;
+//   }
 
-console.log(addUp(4))
+// console.log(addUp(4))
+
+const shiftToLeft = (x, y) => x * 2 ** y;
