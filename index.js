f9d6735bfca93579f2b365371fd22f9ef7edbda0
Akho0122 <mr.akhror.dev@gmail.com>
2021-12-28 22:16:51 +0500

    makesTen function added

--- index.js
+++ index.js
@@ -1,176 +1,181 @@
 
-const arr = [1,2,3,4,54,5,6];
+// const arr = [1,2,3,4,54,5,6];
 
-function sumArr(arr){
-    let sum = 0
-    for(let i = 0; i < arr.length; i++){
-     sum += arr[i]
-    }
-    console.log(sum)
-}
+// function sumArr(arr){
+//     let sum = 0
+//     for(let i = 0; i < arr.length; i++){
+//      sum += arr[i]
+//     }
+//     console.log(sum)
+// }
 
-sumArr(arr);
+// sumArr(arr);
 
-const arr1 = [true, false, true, false, false];
+// const arr1 = [true, false, true, false, false];
 
-function sumTruFalse(arr){
-    let trues = 0;
-    let fal = 0;
-    for(let i = 0; i < arr.length; i++){
-     if(arr[i]){
-     trues++
-    } else{
-        fal++
-    }
-}
-    console.log({trues, fal})
-}
+// function sumTruFalse(arr){
+//     let trues = 0;
+//     let fal = 0;
+//     for(let i = 0; i < arr.length; i++){
+//      if(arr[i]){
+//      trues++
+//     } else{
+//         fal++
+//     }
+// }
+//     console.log({trues, fal})
+// }
 
-sumTruFalse(arr1);
+// sumTruFalse(arr1);
 
-function lessThan100(num1, num2){
-    return (num1 + num2) > 100;
-}
-console.log(lessThan100(12,34));
+// function lessThan100(num1, num2){
+//     return (num1 + num2) > 100;
+// }
+// console.log(lessThan100(12,34));
 
-function lessThan100(num1, num2, num3){
-    return (num1 * 2) + (num2 * 4) + (num3 * 4);
-}
-console.log(lessThan100(2, 3, 5));
+// function lessThan100(num1, num2, num3){
+//     return (num1 * 2) + (num2 * 4) + (num3 * 4);
+// }
+// console.log(lessThan100(2, 3, 5));
 
-function isSameNum(num1, num2){
-    return (num1 === num2);
-}
-console.log(isSameNum(2, 2));
+// function isSameNum(num1, num2){
+//     return (num1 === num2);
+// }
+// console.log(isSameNum(2, 2));
 
-function footballPoints(wins,draws, loses){
-    return (wins * 3) + (draws * 1);
-}
-console.log(footballPoints(5,0,2));
+// function footballPoints(wins,draws, loses){
+//     return (wins * 3) + (draws * 1);
+// }
+// console.log(footballPoints(5,0,2));
 
-function convert(h, m){
-    return (h * 3600) + (m * 60)
-}
-console.log(convert(1,3))
+// function convert(h, m){
+//     return (h * 3600) + (m * 60)
+// }
+// console.log(convert(1,3))
 
-function isSeven(x) {
-	return x === 7;
-}
-console.log(isSeven(7));
+// function isSeven(x) {
+// 	return x === 7;
+// }
+// console.log(isSeven(7));
 
-function profitableGame(prob, prize, pay){
-    return (prob * prize) > pay;
-}
-console.log(profitableGame(0.2, 50, 9))
+// function profitableGame(prob, prize, pay){
+//     return (prob * prize) > pay;
+// }
+// console.log(profitableGame(0.2, 50, 9))
 
-function profitableGame(val){
-    return val + '';
-}
-console.log(profitableGame(2))
+// function profitableGame(val){
+//     return val + '';
+// }
+// console.log(profitableGame(2))
 
-function tipTwo(val) {
-    return val.toString();
-};
-console.log(tipTwo(false));
+// function tipTwo(val) {
+//     return val.toString();
+// };
+// console.log(tipTwo(false));
 
-function addUp(num) {
-    let sum = 0;
-    for (let i = 1; i <= num; i++) {
-      sum += i;
-    }
-    return sum;
-  }
+// function addUp(num) {
+//     let sum = 0;
+//     for (let i = 1; i <= num; i++) {
+//       sum += i;
+//     }
+//     return sum;
+//   }
 
-console.log(addUp(4))
+// console.log(addUp(4))
 
-const shiftToLeft = (x, y) => x * 2 ** y;
-function minMax(arr) {
-    arr.sort(function(a, b){return a-b});
-    return [arr[0], arr[arr.length-1]];
+// const shiftToLeft = (x, y) => x * 2 ** y;
+// function minMax(arr) {
+//     arr.sort(function(a, b){return a-b});
+//     return [arr[0], arr[arr.length-1]];
     
-  }
+//   }
 
-  function timeForMilkAndCookies(date) {
-    date = date.toString().split(' ');
-    return date[1] == 'Dec' && date[2] == 24;
-  }
-  function binary(decimal) {
-    var bin = "";
-    for(; decimal > 1;)
-      {
-        bin = (decimal%2) + bin ;
-        decimal = Math.floor(decimal/2);
-      }
-    bin = decimal + bin ;
-    return bin;
-  }
+//   function timeForMilkAndCookies(date) {
+//     date = date.toString().split(' ');
+//     return date[1] == 'Dec' && date[2] == 24;
+//   }
+//   function binary(decimal) {
+//     var bin = "";
+//     for(; decimal > 1;)
+//       {
+//         bin = (decimal%2) + bin ;
+//         decimal = Math.floor(decimal/2);
+//       }
+//     bin = decimal + bin ;
+//     return bin;
+//   }
 
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
-  const REGEXP = /\d{2}:\d{2}/;
-  let promise = new Promise((resolve, reject) => setTimeout(resolve, 1000, "edabit"));
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+//   const REGEXP = /\d{2}:\d{2}/;
+//   let promise = new Promise((resolve, reject) => setTimeout(resolve, 1000, "edabit"));
 
-  function matchHouses(step) {
-    return step === 0 ? 0 : 5 * step + 1;
-  }
-  function canNest(arr1, arr2) {
-    return Math.min(...arr1) > Math.min(...arr2) && Math.max(...arr1) < Math.max(...arr2);
-  }
-  const numberSquares = n => n > 1? n**2 + numberSquares(n-1) : 1;
+//   function matchHouses(step) {
+//     return step === 0 ? 0 : 5 * step + 1;
+//   }
+//   function canNest(arr1, arr2) {
+//     return Math.min(...arr1) > Math.min(...arr2) && Math.max(...arr1) < Math.max(...arr2);
+//   }
+//   const numberSquares = n => n > 1? n**2 + numberSquares(n-1) : 1;
 
-  function largestSwap(num) {
-    return num / 10 > num % 10
-  }
-  function canNest(arr1, arr2) {
-    return Math.min(...arr1) > Math.min(...arr2) && Math.max(...arr1) < Math.max(...arr2);
-  }
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
-  function whichIsLarger(f, g) {
-    let [fr, gr] = [f(), g()];
-    return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
-  }
+//   function largestSwap(num) {
+//     return num / 10 > num % 10
+//   }
+//   function canNest(arr1, arr2) {
+//     return Math.min(...arr1) > Math.min(...arr2) && Math.max(...arr1) < Math.max(...arr2);
+//   }
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+//   function whichIsLarger(f, g) {
+//     let [fr, gr] = [f(), g()];
+//     return fr > gr ? 'f' : fr < gr ? 'g' : 'neither';
+//   }
+  // function makesTen(a, b){
+  //   return (a + b == 10) || (a === 10 || b === 10);
+  // }
+  // console.log(makesTen(5,5));
+  
