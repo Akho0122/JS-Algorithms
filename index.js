df009e4b0766e04c4c4e3057d6e9db42557cc089
Akho0122 <mr.akhror.dev@gmail.com>
2021-12-30 23:30:50 +0500

    New Task About arrays was added

--- index.js
+++ index.js
@@ -134,4 +134,7 @@
 //   function matchHouses(step) {
 //     return step === 0 ? 0 : 5 * step + 1;
 //   }
+//   function matchHouses(step) {
+//     return step === 0 ? 0 : 5 * step + 1;
+//   }
 
